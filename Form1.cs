ee336e36d53166841f32b0427fc83d69c9b9f8c1
master16mh <master16.mh@gmail.com>
2024-09-30 16:15:02 -0300

    Fix de mensaje

--- Form1.cs
+++ Form1.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Evaluación1c_
 {
@@ -30,21 +21,17 @@ namespace Evaluación1c_
 
             }
 
-
             //asignar a la variable el valor de lo que ingresemos en el cuadro de texto
             lista = Convert.ToString(textBox1.Text);
             //agregamos lo que tenga la variable valor al listbox
             listBox1.Items.Add(lista);
-            //limpie items de listbox2
+            // limpiamos el listBox2 
             listBox2.Items.Clear();
-
 
             //generamos ciclo for que empiece desde 1 para que cuente desde 1 
             for (int i = 1; i <= listBox1.Items.Count; i++)
             {
-               
                 listBox2.Items.Add(i.ToString());
-                
             }
             //limpiar el cuadro de texto y darle el enfoque
             textBox1.Text = "";
@@ -67,7 +54,7 @@ namespace Evaluación1c_
             valor1 = Convert.ToInt32(textBox1.Text); //lo que ingresamos en cuadro de texto sera el indice
             if (valor1 > listBox1.Items.Count - 1)
             {
-                MessageBox.Show ("El valor ingresado está fuera del índice");
+                MessageBox.Show("El valor ingresado está fuera del índice");
                 textBox1.Text = "";
                 textBox1.Focus();
             }
@@ -91,10 +78,16 @@ namespace Evaluación1c_
         {
             var ok = listBox1.SelectedItem;
             string item;
-            
+
+            if (listBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Debe seleccionar un item");
+                return;
+            }
+
             item = Convert.ToString(listBox1.SelectedItem);
             listBox2.Items.Remove(listBox1.Items.Count.ToString());
-            listBox1.Items.Remove(item);     
+            listBox1.Items.Remove(item);
             textBox1.Text = "";
             listBox1.Focus();
         }
